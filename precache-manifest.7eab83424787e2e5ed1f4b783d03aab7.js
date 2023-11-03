f7dd4d5bbbd72e0a377db3dd8e058c0b4326a9ed
TechNomad4 <kamaudavis3022@gmail.com>
2023-11-03 13:36:16 +0300

    Updates

---
+++ precache-manifest.7eab83424787e2e5ed1f4b783d03aab7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3da31a7776778b30890ef37d37f8e922",
+    "url": "/Tasklist/index.html"
+  },
+  {
+    "revision": "0eb096bc673b2cbe4496",
+    "url": "/Tasklist/static/css/main.f7536392.chunk.css"
+  },
+  {
+    "revision": "8fd46ef0da1b67668278",
+    "url": "/Tasklist/static/js/2.cf0c1b63.chunk.js"
+  },
+  {
+    "revision": "0eb096bc673b2cbe4496",
+    "url": "/Tasklist/static/js/main.d34b5a6f.chunk.js"
+  },
+  {
+    "revision": "9b20994ce9733d4d0c63",
+    "url": "/Tasklist/static/js/runtime-main.ef0bf631.js"
+  }
+]);
